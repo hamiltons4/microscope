98557e5fee88cf4e215c456c2eb36c7c775508ac
Hamilton Smith <smithhamilton4@gmail.com>
2015-11-29 18:38:12 -0500

    Discover Meteor: Microscope

---
+++ client/templates/posts/post_list.js
@@ -0,0 +1,26 @@
+/*var postsData = [
+   {
+   	 title: 'Introducing Telescope',
+   	 url: 'http://sachagreif.com/Introducing-telescope/'
+   },
+   {
+   	 title: 'Meteor',
+   	 url: 'http://meteor.com'
+   },
+   {
+   	 title: 'The Meteor Book',
+   	 url: 'http://themeteorbook.com'
+   }
+
+
+];
+Template.postsList.helpers({
+	posts: postsData
+});
+
+*/
+Template.postsList.helpers({
+	posts: function() {
+		return Posts.find();
+	}
+})
